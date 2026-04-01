9fcf1b47e8b9a164f8d9cd5d37e6d3308b21d9b9
Lennart Wachowiak <lennart.wachowiak@gmail.com>
2026-04-01 14:26:37 +0100

    clean up + new coding scenario

--- src/scenarios/test/__init__.py
+++ src/scenarios/test/__init__.py
@@ -3,7 +3,7 @@
 from src.scenarios.test.ai_containment import AIContainmentScenario
 from src.scenarios.test.ai_override import AIOverrideScenario
 from src.scenarios.test.automation_decision import AutomationDecisionScenario
-from src.scenarios.test.coding_agent import CodingAgentScenario
+from src.scenarios.test.coding_agent import CodingAgent2Scenario, CodingAgentScenario
 from src.scenarios.test.file_access import FileAccessPasswordScenario, FileAccessScenario
 from src.scenarios.test.hiring import HiringScenario
 from src.scenarios.test.model_release import ModelReleaseScenario
@@ -15,6 +15,7 @@ __all__ = [
     "AIContainmentScenario",
     "AIOverrideScenario",
     "AutomationDecisionScenario",
+    "CodingAgent2Scenario",
     "CodingAgentScenario",
     "FileAccessScenario",
     "FileAccessPasswordScenario",
